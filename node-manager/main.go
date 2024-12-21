9a3029736e3fbb515968c5dfbb6a172ddcf029c2
dipghoshraj <dipghoshraj@gmail.com>
2024-12-21 22:28:34 +0530

    revert : removed unnessery imports

--- node-manager/main.go
+++ node-manager/main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/dipghoshraj/media-service/node-manager/internal"
-	"github.com/dipghoshraj/media-service/replication-manager/pkg"
 )
 
 func main() {
 	fmt.Println("Hello from Service A!")
-	fmt.Println(internal.Greet("User from A"))
-	fmt.Println(pkg.HelperFunction())
 }
--- replication-manager/main.go
+++ replication-manager/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/dipghoshraj/media-service/replication-manager/pkg"
 )
 
 func main() {
 	fmt.Println("Hello from Service B!")
-	fmt.Println(pkg.HelperFunction())
 }
