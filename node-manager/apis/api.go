98b63393c0596304d8f17231dd1520e7a0579072
dipghoshraj <dipghoshraj@gmail.com>
2024-12-27 00:50:25 +0530

    feat : added response json loggin meddleware

--- node-manager/apis/api.go
+++ node-manager/apis/api.go
@@ -1,6 +1,11 @@
 package apis
 
 import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+
 	"github.com/dipghoshraj/media-service/node-manager/model"
 	"github.com/gorilla/mux"
 )
@@ -17,4 +22,43 @@ func SetupRoutes(router *mux.Router, nm *NMHandler) {
 
 	router.Use(loggingMiddleware)
 	router.Use(recoveryMiddleware)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, APIResponse{
+		Success: false,
+		Error:   message,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf(
+			"%s %s %s",
+			r.Method,
+			r.RequestURI,
+			time.Since(start),
+		)
+	})
+}
+
+func recoveryMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				log.Printf("panic: %v", err)
+				respondWithError(w, http.StatusInternalServerError, "Internal server error")
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
 }
--- node-manager/apis/nodeApi.go
+++ node-manager/apis/nodeApi.go
@@ -3,7 +3,6 @@ package apis
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"time"
 
@@ -118,44 +117,5 @@ func (nm *NMHandler) GetClusterStatsHandler(w http.ResponseWriter, r *http.Reque
 	respondWithJSON(w, http.StatusOK, APIResponse{
 		Success: true,
 		Data:    stats,
-	})
-}
-
-func respondWithError(w http.ResponseWriter, code int, message string) {
-	respondWithJSON(w, code, APIResponse{
-		Success: false,
-		Error:   message,
-	})
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	response, _ := json.Marshal(payload)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
-}
-
-func loggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		next.ServeHTTP(w, r)
-		log.Printf(
-			"%s %s %s",
-			r.Method,
-			r.RequestURI,
-			time.Since(start),
-		)
-	})
-}
-
-func recoveryMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer func() {
-			if err := recover(); err != nil {
-				log.Printf("panic: %v", err)
-				respondWithError(w, http.StatusInternalServerError, "Internal server error")
-			}
-		}()
-		next.ServeHTTP(w, r)
 	})
 }
