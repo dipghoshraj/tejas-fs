1c62fdae2830b57a772030d82e85f892325e06e5
dipghoshraj <dipghoshraj@gmail.com>
2025-01-09 01:38:23 +0530

    feat : added healthcheck based on GRPC

---
+++ file-service-nodes/domain/healthcheck.go
@@ -0,0 +1,94 @@
+package domain
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/dipghoshraj/media-service/file-service-nodes/domain/proto"
+)
+
+type StorageServer struct {
+	proto.UnimplementedStorageBoxServer
+}
+
+func convertSizeToMB(size string) (int64, error) {
+	// Define regex to match size values with units (e.g., 1.5G, 500M, 1024K)
+	re := regexp.MustCompile(`([0-9.]+)([KMG]?)`)
+	matches := re.FindStringSubmatch(size)
+
+	if len(matches) < 3 {
+		return 0, fmt.Errorf("unable to parse size: %s", size)
+	}
+
+	// Extract the numeric value and the unit
+	valueStr := matches[1]
+	unit := matches[2]
+
+	// Convert the numeric value to float
+	value, err := strconv.ParseFloat(valueStr, 64)
+	if err != nil {
+		return 0, fmt.Errorf("error parsing value: %v", err)
+	}
+
+	// Convert the value to MB based on the unit
+	switch unit {
+	case "K":
+		// KB to MB
+		return int64(value / 1024), nil
+	case "M":
+		// Already in MB
+		return int64(value), nil
+	default:
+		// If no unit, assume the value is in MB
+		return int64(value), nil
+	}
+}
+
+func GetUsedSpace() (int64, error) {
+
+	var out bytes.Buffer
+	volumePath := "/data"
+
+	cmd := exec.Command("du", "-sh", volumePath)
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return 0, fmt.Errorf("error executing du command: %v", err)
+	}
+
+	duOutput := out.String()
+	duParts := strings.Fields(duOutput)
+
+	if len(duParts) < 1 {
+		return 0, fmt.Errorf("invalid output from du command")
+	}
+
+	// Parse the size and convert it to MB
+	size := duParts[0]
+	sizeInMB, err := convertSizeToMB(size)
+	if err != nil {
+		return 0, fmt.Errorf("error converting size to MB: %v", err)
+	}
+
+	// Return the size in MB
+	return sizeInMB, nil
+}
+
+func (s *StorageServer) HealthCheck(ctx context.Context, req *proto.HealthCheckMessage) (*proto.HelloReply, error) {
+	usedSapce, err := GetUsedSpace()
+	if err != nil {
+		return &proto.HelloReply{
+			Status:    "inactive",
+			UsedSpace: 0,
+		}, nil
+	}
+	return &proto.HelloReply{
+		Status:    "active",
+		UsedSpace: usedSapce,
+	}, nil
+}
---
+++ file-service-nodes/domain/ingress.go
@@ -0,0 +1,11 @@
+package domain
+
+// "github.com/dipghoshraj/media-service/file-service-nodes/domain/storage"
+
+// protoc --go-grpc_out=. --go-grpc_opt=paths=source_relative file-service-nodes\domain\proto\storage.proto
+
+// func (s *StorageServer) SayHello(ctx context.Context, req *proto.HelloWorld) (*proto.HelloReply, error) {
+// 	return &proto.HelloReply{
+// 		Message: "Hello " + req.GetName(),
+// 	}, nil
+// }
