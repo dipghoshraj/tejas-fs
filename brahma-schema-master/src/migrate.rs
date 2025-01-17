673e98539e993944cace0a3524c5355a01a5559c
dipghoshraj <dipghoshraj@gmail.com>
2025-01-18 02:15:31 +0530

    feat : added the migrator

---
+++ brahma-schema-master/src/migrate.rs
@@ -0,0 +1,24 @@
+use postgres::{Client, Error};
+use std::fs;  // Synchronous file handling from std
+
+
+
+fn get_migration_file_path(file_name:  &mut String) -> String {
+    format!("migrations/{}.sql", file_name)  // Returns the full path to the SQL file
+}
+
+
+fn apply_single_migration(client: &mut Client, file_name:  &mut String)-> Result<(), Error> {
+
+    let file_apth = get_migration_file_path(file_name);
+
+    let sql = fs::read_to_string(file_apth).expect("Failed to read migration file");
+    client.batch_execute(&sql)?;
+    Ok(())
+    
+
+}
+
+pub fn apply_migration(client: &mut Client, file_name: &mut String) -> Result<(), Error> {
+    apply_single_migration(client, file_name)
+}
