7f59e544eef401e146c3a6504ae56ed018b22c70
dipghoshraj <dipghoshraj@gmail.com>
2025-01-18 02:39:20 +0530

    feat : main loading the env

--- planet-of-nodes/commander/main.go
+++ planet-of-nodes/commander/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	PostgresHost     string
+	PostgresPort     string
+	PostgresUser     string
+	PostgresPassword string
+	PostgresDB       string
+	RedisHost        string
+	RedisPort        string
+	RedisPassword    string
+	ServerPort       string
+}
+
+func getEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
+
+func loadConfig() (*Config, error) {
+	// Load .env file if it exists
+	godotenv.Load()
+
+	config := &Config{
+		PostgresHost:     getEnv("POSTGRES_HOST", "localhost"),
+		PostgresPort:     getEnv("POSTGRES_PORT", "5432"),
+		PostgresUser:     getEnv("POSTGRES_USER", "postgres"),
+		PostgresPassword: getEnv("POSTGRES_PASSWORD", ""),
+		PostgresDB:       getEnv("POSTGRES_DB", ""),
+		RedisHost:        getEnv("REDIS_HOST", "localhost"),
+		RedisPort:        getEnv("REDIS_PORT", "6379"),
+		RedisPassword:    getEnv("REDIS_PASSWORD", ""),
+		ServerPort:       getEnv("SERVER_PORT", "8080"),
+	}
+
+	return config, nil
+}
 
 func main() {
-	fmt.Println("starting here")
+	fmt.Println("here starts the creation")
+
+	config, err := loadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load configuration: %v", err)
+	}
+	fmt.Println(config)
+
 }
