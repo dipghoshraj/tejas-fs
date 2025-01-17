c5005543d258bb6d2a1150c67754c987955b8c95
dipghoshraj <dipghoshraj@gmail.com>
2025-01-18 02:36:26 +0530

    feat : added the model for node

---
+++ planet-of-nodes/cosmic-model/granth.go
@@ -0,0 +1,11 @@
+package cosmicmodel
+
+type Granths struct {
+	ID            string
+	Name          string
+	Size          int64
+	TotalSegments int
+	IngressNodeId string
+	Distributed   bool
+	Ext           string
+}
---
+++ planet-of-nodes/cosmic-model/node.go
@@ -0,0 +1,25 @@
+package cosmicmodel
+
+import "time"
+
+type NodeStatus string
+
+const (
+	NodeStatusActive   NodeStatus = "active"
+	NodeStatusFailed   NodeStatus = "failed"
+	NodeStatusPending  NodeStatus = "pending"
+	NodeStatusInactive NodeStatus = "inactive"
+)
+
+type LokaNode struct {
+	ID            string     `json:"id" gorm:"primaryKey"`
+	Status        NodeStatus `json:"status"`
+	Capacity      int64      `json:"capacity"`  // in MB
+	UsedSpace     int64      `json:"usedSpace"` // in MB
+	LastHeartbeat time.Time  `json:"lastHeartbeat"`
+	CreatedAt     time.Time  `json:"createdAt"`
+	UpdatedAt     time.Time  `json:"updatedAt"`
+	VolumeName    string     `json:"volumeName"`
+	Port          string     `json:"port"`
+	Host          string     `json:"host"`
+}
