6b9b4d6879b2a06a61bddd3b6b8d6b6cb6bd8b3d
dipghoshraj <dipghoshraj@gmail.com>
2025-01-22 15:22:30 +0530

    feat : added the node spinup controller

--- planet-of-nodes/handler/spinner.go
+++ planet-of-nodes/handler/spinner.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"fmt"
 	cosmicmodel "planet-of-node/cosmic-model"
+	nodeunit "planet-of-node/node-unit"
 )
 
 type NodeStatus string
@@ -14,6 +16,30 @@ const (
 
 func (hm *hManager) SpinUpContainer(node *cosmicmodel.Node) error {
 	// start the transcation operation
+	tx := hm.dbm.DB.Begin()
+	// crate the node metadata in theDB
+	if err := tx.Create(node).Error; err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to update node port: %v", err)
+	}
+
+	port, err := nodeunit.AllocatePort(hm.dbm.RedisClient)
+	if err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to update node port: %v", err)
+	}
+
+	node.Port = port
+
+	if err := nodeunit.SpinUpContainer(node.ID, node.VolumeName, node.Capacity, node.Port); err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to update node port: %v", err)
+	}
+
+	if err := tx.Save(node).Error; err != nil {
+		tx.Rollback()
+		return fmt.Errorf("failed to update node port: %v", err)
+	}
 
 	return nil
 }
