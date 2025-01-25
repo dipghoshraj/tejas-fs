93142432262980fc81a42cd08fba1ac49cb7f655
dipghoshraj <dipghoshraj@gmail.com>
2025-01-26 01:30:22 +0530

    feat : cluster api request dto

--- planet-of-nodes/api/clusterController.go
+++ planet-of-nodes/api/clusterController.go
@@ -1,20 +1,49 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
-type Cluster struct {
-	Nodes        int64 `json:"nodes"`
-	NodecCpacity int64 `json:"node_capacity"`
-	IngressNode  int64 `json:"ingress_node"`
-	AutoScale    bool  `json:"auto_scale"`
+	cosmicmodel "planet-of-node/cosmic-model"
+
+	"github.com/google/uuid"
+)
+
+type ClusterRequest struct {
+	Nodes       int64 `json:"nodes"`
+	NodeCpacity int64 `json:"node_capacity"`
+	IngressNode int64 `json:"ingress_node"`
+	AutoScale   bool  `json:"auto_scale"`
 }
 
-type Response struct{}
+type Response struct {
+	clsuter interface{}
+}
 
 func (na *NApi) CreateCluster(w http.ResponseWriter, r *http.Request) {
 
+	var request ClusterRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		respondError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	cluster_id := uuid.New().String()
+	cluster := &cosmicmodel.ClusterConsfig{
+		ID:           cluster_id,
+		Name:         "cluster-" + cluster_id,
+		Nodes:        request.Nodes,
+		NodeCapacity: request.NodeCpacity,
+		AutoScaling:  request.AutoScale,
+		IngressNodes: request.IngressNode,
+	}
+
+	// TODO: Save the cluster metadata and create the cluster in backgroud
+
 	respondWithJson(w, http.StatusOK, APIResponse{
 		Success: true,
-		Data:    Response{},
+		Data: Response{
+			clsuter: cluster,
+		},
 	})
 }
