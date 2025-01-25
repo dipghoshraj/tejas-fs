128852d1b84ff61fcc2ce0d198dea62a4a5e4f78
dipghoshraj <dipghoshraj@gmail.com>
2025-01-26 01:03:28 +0530

    feat : added config and redis initialization

--- planet-of-nodes/commander/main.go
+++ planet-of-nodes/commander/main.go
@@ -4,25 +4,11 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
+
+	nodeunt "planet-of-node/node-unit"
 
 	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
-
-type Config struct {
-	PostgresHost     string
-	PostgresPort     string
-	PostgresUser     string
-	PostgresPassword string
-	PostgresDB       string
-	RedisHost        string
-	RedisPort        string
-	RedisPassword    string
-	ServerPort       string
-}
 
 func getEnv(key, defaultValue string) string {
 	value := os.Getenv(key)
@@ -32,11 +18,11 @@ func getEnv(key, defaultValue string) string {
 	return value
 }
 
-func loadConfig() (*Config, error) {
+func loadConfig() (*nodeunt.Config, error) {
 	// Load .env file if it exists
 	godotenv.Load()
 
-	config := &Config{
+	config := &nodeunt.Config{
 		PostgresHost:     getEnv("POSTGRES_HOST", "localhost"),
 		PostgresPort:     getEnv("POSTGRES_PORT", "5432"),
 		PostgresUser:     getEnv("POSTGRES_USER", "postgres"),
@@ -49,47 +35,6 @@ func loadConfig() (*Config, error) {
 	}
 
 	return config, nil
-}
-
-func initializeDB(config *Config) (*gorm.DB, error) {
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		config.PostgresHost,
-		config.PostgresPort,
-		config.PostgresUser,
-		config.PostgresPassword,
-		config.PostgresDB,
-	)
-
-	// Try to connect with retries
-	var db *gorm.DB
-	var err error
-	for i := 0; i < 5; i++ {
-		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
-			Logger: logger.Default.LogMode(logger.Info),
-		})
-		if err == nil {
-			break
-		}
-		log.Printf("Failed to connect to database, attempt %d/5: %v", i+1, err)
-		time.Sleep(time.Second * 5)
-	}
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to database after 5 attempts: %v", err)
-	}
-
-	// Get underlying SQL DB to set connection pool settings
-	sqlDB, err := db.DB()
-	if err != nil {
-		return nil, err
-	}
-
-	// Set connection pool settings
-	sqlDB.SetMaxIdleConns(10)
-	sqlDB.SetMaxOpenConns(100)
-	sqlDB.SetConnMaxLifetime(time.Hour)
-
-	return db, nil
 }
 
 func main() {
--- planet-of-nodes/cosmic-model/init.go
+++ planet-of-nodes/cosmic-model/init.go
@@ -2,10 +2,17 @@ package cosmicmodel
 
 import (
 	"context"
+	"fmt"
+	"log"
 	"sync"
+	"time"
+
+	nodeunt "planet-of-node/node-unit"
 
 	"github.com/go-redis/redis/v8"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type DBM struct {
@@ -27,4 +34,64 @@ func ModelManager(db *gorm.DB, redisClient *redis.Client, ipPool string) *DBM {
 		RedisClient: redisClient,
 		ctx:         context.Background(),
 	}
+}
+
+func InitializeDB(config *nodeunt.Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		config.PostgresHost,
+		config.PostgresPort,
+		config.PostgresUser,
+		config.PostgresPassword,
+		config.PostgresDB,
+	)
+
+	// Try to connect with retries
+	var db *gorm.DB
+	var err error
+	for i := 0; i < 5; i++ {
+		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+			Logger: logger.Default.LogMode(logger.Info),
+		})
+		if err == nil {
+			break
+		}
+		log.Printf("Failed to connect to database, attempt %d/5: %v", i+1, err)
+		time.Sleep(time.Second * 5)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database after 5 attempts: %v", err)
+	}
+
+	// Get underlying SQL DB to set connection pool settings
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	// Set connection pool settings
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	return db, nil
+}
+
+func InitializeRedis(config *nodeunt.Config) (*redis.Client, error) {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", config.RedisHost, config.RedisPort),
+		Password: config.RedisPassword,
+		DB:       0,
+	})
+
+	// Test connection
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to Redis: %v", err)
+	}
+
+	return redisClient, nil
 }
