02f3b62dd5bff22f10de64f22adc2d920a153b42
dipghosh <dip.ghosh@procore.com>
2025-03-23 16:47:01 +0530

    feat : added the config details

--- agni-stack/clusterapp/src/main.rs
+++ agni-stack/clusterapp/src/main.rs
@@ -1,16 +1,22 @@
-use sysinfo::System;
+// Code: src/main.rs
+// Cargo.toml
+// [package]
+// name = "clusterApp"
+// version = "0.1.0"
+// edition = "2025"
+//
+
+
+mod config;
+mod process;
+
+
+use env_logger;
 
 
 fn main() {
-    let mut sys = System::new();
-
-    loop {
-        sys.refresh_cpu_usage(); // Refreshing CPU usage.
-        for cpu in sys.cpus() {
-            print!("{}% ", cpu.cpu_usage());
-        }
-        // Sleeping to let time for the system to run for long
-        // enough to have useful information.
-        std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
-    }
+    env_logger::init();
+    
+    let config = config::ConsumerConfig::load();
+    println!("Starting consumer with config: {:?}", config);
 }
---
+++ agni-stack/clusterapp/src/process.rs
@@ -0,0 +1,6 @@
+use log::info;
+
+
+pub fn process_message(message: &str){
+    info!("Processing message: {}", message);
+}
