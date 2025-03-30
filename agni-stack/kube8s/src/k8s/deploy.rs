ed92884b53e4db1868a886b0f869cec9beb2bd67
dipghoshraj <dipghoshraj@gmail.com>
2025-03-30 23:37:08 +0530

    added the k8s deployment

---
+++ agni-stack/kube8s/src/k8s/deploy.rs
@@ -0,0 +1,8 @@
+
+
+pub fn deployk8s (){
+    println!("Deploying to Kubernetes...");
+    // Add your Kubernetes deployment logic here
+    // This could involve using a library like kube-rs to interact with the Kubernetes API
+    // or using command-line tools like kubectl to apply your deployment manifests.
+}
--- agni-stack/kube8s/src/main.rs
+++ agni-stack/kube8s/src/main.rs
@@ -1,3 +1,8 @@
+mod k8s;
+
+use k8s::deploy::deployk8s;
+
 fn main() {
     println!("Hello, world!");
+    deployk8s();
 }
