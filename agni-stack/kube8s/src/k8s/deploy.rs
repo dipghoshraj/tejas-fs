25de1c515c347d4b5c0d969a3f9f86377f01dd70
dipghoshraj <dipghoshraj@gmail.com>
2025-03-31 00:10:48 +0530

    feat : spawn one pod with deployment

--- agni-stack/kube8s/src/k8s/deploy.rs
+++ agni-stack/kube8s/src/k8s/deploy.rs
@@ -1,8 +1,46 @@
 
 
-pub fn deployk8s (){
-    println!("Deploying to Kubernetes...");
-    // Add your Kubernetes deployment logic here
-    // This could involve using a library like kube-rs to interact with the Kubernetes API
-    // or using command-line tools like kubectl to apply your deployment manifests.
+use kube::{
+    api::{Api, PostParams},
+    Client
+};
+
+use k8s_openapi::api::apps::v1::Deployment;
+use serde_json::json;
+
+pub async fn deployk8s (appname: &str, image: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::try_default().await.unwrap();
+    let deployments: Api<Deployment> = Api::namespaced(client, "default");
+
+    let deployment = json!({
+        "apiVersion": "apps/v1",
+        "kind": "Deployment",
+        "metadata": {
+            "name": appname,
+        },
+        "spec": {
+            "replicas": 1,
+            "selector": {
+                "matchLabels": { "app": appname }
+            },
+            "template": {
+                "metadata": {
+                    "labels": { "app": appname }
+                },
+                "spec": {
+                    "containers": [{
+                        "name": appname,
+                        "image": image,
+                        "ports": [{"containerPort": 80}]
+                    }]
+                }
+            }
+        }
+    });
+
+    let deploy: Deployment = serde_json::from_value(deployment)?;
+    deployments.create(&PostParams::default(), &deploy).await?;
+
+    println!("✅ Successfully deployed {}", appname);
+    Ok(())
 }
--- agni-stack/kube8s/src/main.rs
+++ agni-stack/kube8s/src/main.rs
@@ -2,7 +2,16 @@ mod k8s;
 
 use k8s::deploy::deployk8s;
 
-fn main() {
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
-    deployk8s();
+
+    let appname = "myapp";
+    let image = "dipghoshraj/omnia-ar-app:0.0.7";
+    let result = deployk8s(appname, image).await;
+    match result {
+        Ok(_) => println!("Deployment successful"),
+        Err(e) => eprintln!("Error deploying: {}", e),
+    }
 }
