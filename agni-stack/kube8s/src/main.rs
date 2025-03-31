ff1fa7104d5f7f88a7ec22fae1136e473c9b6d63
dipghoshraj <dipghoshraj@gmail.com>
2025-03-31 23:56:53 +0530

    AGNI-002 : feat : added the network and services call

--- agni-stack/kube8s/src/main.rs
+++ agni-stack/kube8s/src/main.rs
@@ -1,6 +1,8 @@
 mod k8s;
 
 use k8s::deploy::deployk8s;
+use k8s::network::create_ingress;
+use k8s::service::create_service;
 
 
 #[tokio::main]
@@ -11,7 +13,21 @@ async fn main() {
     let image = "dipghoshraj/omnia-ar-app:0.0.7";
     let result = deployk8s(appname, image).await;
     match result {
-        Ok(_) => println!("Deployment successful"),
+        Ok(_) => {
+            println!("Deployment successful");
+            let result = create_service(appname, 80).await;
+            match result {
+                Ok(_) => {
+                    println!("Service created successfully");
+                    let result = create_ingress(appname, "myapp.example.com").await;
+                    match result {
+                        Ok(_) => println!("Ingress created successfully"),
+                        Err(e) => eprintln!("Error creating ingress: {}", e),
+                    }
+                }
+                Err(e) => eprintln!("Error creating service: {}", e),
+            }
+        },
         Err(e) => eprintln!("Error deploying: {}", e),
     }
 }
